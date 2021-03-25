6c1a84ad20a82ce355482235743527fc67c3f659
nojaf <florian.verdonck@outlook.com>
2021-03-25 14:06:56 +0100

    FCS 40 preview

--- fsharp/prerelease-fcs.fsx
+++ fsharp/prerelease-fcs.fsx
@@ -1,9 +1,9 @@
 #i "nuget:https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-tools/nuget/v3/index.json"
-#r "nuget:FSharp.Compiler.Service, 39.0.3-preview.21102.10"
+#r "nuget:FSharp.Compiler.Service, 39.0.0-preview.21174.2"
 
 open FSharp.Compiler.Text
-open FSharp.Compiler.SyntaxTree
-open FSharp.Compiler.SourceCodeServices
+open FSharp.Compiler.Syntax
+open FSharp.Compiler.CodeAnalysis
 
 let fileName = "tmp.fsx"
 let checker = FSharpChecker.Create()
@@ -14,15 +14,17 @@ let parsingOptions =
 
 let source =
     """
-    let s = @"x"
-    let x - 0
+namespace global
+
+let s = @"x"
+let x - 0
     """
     |> SourceText.ofString
 
 let ast =
     async {
         let! tree = checker.ParseFile(fileName, source, parsingOptions)
-        return tree.ParseTree.Value
+        return tree.ParseTree
     }
     |> Async.RunSynchronously
 
