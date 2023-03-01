3a891e6020b0ba7607185c9ad0450b725aa2d7a6
nojaf <florian.verdonck@outlook.com>
2023-03-01 13:13:30 +0100

    Add named fields sample

---
+++ fsharp/sample.fsx
@@ -0,0 +1,8 @@
+﻿#r "nuget: Fantomas.Core, 6.0.0-alpha-005"
+
+open FSharp.Compiler.Syntax
+
+let visit (e: SynExpr) =
+    match e with
+    | SynExpr.ObjExpr(objType = objType; argOptions = args; withKeyword = mWith) -> ()
+    | _ -> failwith "todo"
