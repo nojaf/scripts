42cb0c38a03c5492f6da27f55427d6be169d93c7
nojaf <florian.verdonck@outlook.com>
2023-04-26 07:58:26 +0200

    Before Norway

--- fsharp/gold.fsx
+++ fsharp/gold.fsx
@@ -5,49 +5,27 @@ open System.IO
 open TextCopy
 
 let targetFolder =
-    @"C:\Users\nojaf\Projects\resharper-fsharp\ReSharper.FSharp\test\data\features\completion"
+    @"C:\Users\nojaf\Projects\resharper-fsharp\ReSharper.FSharp\test\data\features\generate\signatureFiles"
 
 let (</>) a b = Path.Combine(a, b)
 
 let mkTest
     testName
     (implContent: string)
-    //(signatureContentBefore: string)
-    // (signatureContentAfter: string)
     =
-    let implContent = implContent.Trim()
-    // let signatureContentBefore = signatureContentBefore.Trim()
-    // let signatureContentAfter = signatureContentAfter
     File.WriteAllText(targetFolder </> $"{testName}.fs", implContent)
-    // File.WriteAllText(targetFolder </> $"{testName}.fs.gold", implContent)
-    // File.WriteAllText(targetFolder </> $"{testName}.fsi", signatureContentBefore)
-    // File.WriteAllText(targetFolder </> $"{testName}.fsi.gold", signatureContentAfter)
-
     ClipboardService.SetText($"[<Test>] member x.``{testName}`` () = x.DoNamedTest()")
 
 mkTest
-    "NamedUnionCaseFieldsPat - 05"
-    """// ${COMPLETE_ITEM:banana}
-// ${COMPLETE_ITEM:citrus}
+    "Nested module 01"
+    """// ${KIND:SignatureFile}
+// ${SELECT0:Generate signature file title}
 module Foo
 
-type Foo =
-    | Meh of int * string
-    | Bar of apple:int * banana: string * citrus: float
-
-let a (b: Foo) =
-    match b with
-    | Bar(a = apple; {caret}) ->
+    module Bar =
+        open System
+{caret}
 """
-
-Directory.EnumerateFiles(targetFolder)
-|> Seq.iter (fun path ->
-    if path.Contains("NamedUnionCaseFieldsPat") then
-        let content = File.ReadAllText(path)
-
-        if content.[content.Length - 1] <> '\n' then
-            File.WriteAllText(path, String.Concat(content, '\n'))
-)
 
 Directory.EnumerateFiles(targetFolder, "*.tmp")
 |> Seq.iter (fun path ->
