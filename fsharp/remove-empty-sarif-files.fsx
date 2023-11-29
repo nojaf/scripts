4e89e4004feb949d0e9449a8569c73386c3d2295
nojaf <florian.verdonck@outlook.com>
2023-11-29 15:46:18 +0100

    Remove empty sarif files.

---
+++ fsharp/remove-empty-sarif-files.fsx
@@ -0,0 +1,32 @@
+﻿// A sarif could actually be empty, the following code cleans up the result folder.
+#r "nuget: Thoth.Json.Net, 11.0.0"
+
+open System.IO
+open Thoth.Json.Net
+
+let anyDecoder = Decode.object (fun _ -> true)
+
+let sarifDecoder =
+    Decode.object (fun get ->
+        let runs =
+            get.Required.Field
+                "runs"
+                (Decode.array (Decode.object (fun get -> get.Required.Field "results" (Decode.array anyDecoder))))
+
+        (Array.concat runs).Length
+    )
+
+let removeEmptySarifFiles (analysisResultFolder: string) =
+    Directory.EnumerateFiles(analysisResultFolder, "*.sarif")
+    |> Seq.iter (fun file ->
+        let json = File.ReadAllText file
+
+        match Decode.fromString sarifDecoder json with
+        | Error err -> printfn "%s" err
+        | Ok count ->
+            if count = 0 then
+                printfn $"Removing %s{file}"
+                File.Delete file
+    )
+
+removeEmptySarifFiles @"C:\Users\nojaf\Projects\Fable\src\reports"
