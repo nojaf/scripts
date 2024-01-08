8536c26a9f39c34dbb5e2dbc8ac1f7467a9eb50b
nojaf <florian.verdonck@outlook.com>
2024-01-08 15:42:02 +0100

    Correctly process timings csv.

--- fsharp/longest-path.fsx
+++ fsharp/longest-path.fsx
@@ -1,10 +1,13 @@
-﻿#r @"C:\Users\nojaf\Projects\Graphoscope\src/Graphoscope/bin/Debug/netstandard2.0/Graphoscope.dll"
+﻿#r "nuget: FSharp.Data, 6.3.0"
+#r @"C:\Users\nojaf\Projects\Graphoscope\src/Graphoscope/bin/Debug/netstandard2.0/Graphoscope.dll"
 #load "./common.fsx"
 
 open System
 open System.Collections.Generic
 open System.IO
 open System.Text.RegularExpressions
+open FSharp.Data
+open FSharp.Data.CsvExtensions
 open Graphoscope
 open Graphoscope.Measures
 open Common
@@ -23,6 +26,7 @@ type LongestPathResult =
     member this.SlowestFile =
         this.LongestPath
         |> List.take (this.LongestPath.Length - 1)
+        |> List.filter (fun n -> n.FileName.EndsWith(".fs", StringComparison.Ordinal))
         |> List.maxBy (fun n -> n.Duration.TotalMilliseconds)
 
     member this.MermaidStyle =
@@ -38,23 +42,8 @@ and NodeInfo =
         Duration: TimeSpan
     }
 
-let processProject (fsprojFile: FileInfo) =
-    // Create the rsp file
-    let rspFile = Path.ChangeExtension(fsprojFile.FullName, ".rsp") |> FileInfo
-
-    if not rspFile.Exists then
-        failwith $"No rsp file was found for {fsprojFile.FullName}"
-
-    let graph =
-        match tryFindOutputFlag rspFile with
-        | None -> failwith $"no --output flag found in %s{rspFile.FullName}"
-        | Some outputFlag ->
-
-        let outputPath = Path.GetFullPath(outputFlag, fsprojFile.DirectoryName)
-        Path.ChangeExtension(outputPath, ".graph.md") |> FileInfo
-
-    assert graph.Exists
-
+/// <param name="projectDirectory">Directory where the fsproj resides.</param>
+let processReportAndGraph (projectDirectory: DirectoryInfo) (timingCsv: FileInfo) (graph: FileInfo) =
     let graphLines = File.ReadAllLines(graph.FullName)
 
     let fileNamesMap =
@@ -65,33 +54,46 @@ let processProject (fsprojFile: FileInfo) =
             if m.Success then
                 let index = int m.Groups.[1].Value
                 let name = m.Groups.[2].Value
-                Some(index, name)
+                let fullPath = Path.Combine(projectDirectory.FullName, name)
+                Some(index, fullPath)
             else
                 None
 
         graphLines |> Array.choose parseInput |> dict
 
-    let timingCsv = Path.Combine(fsprojFile.DirectoryName, "graph.csv") |> FileInfo
-
     if not timingCsv.Exists then
         failwith $"No timing csv found at %s{timingCsv.FullName}"
 
     let timings =
-        File.ReadAllLines(timingCsv.FullName)
-        |> Array.choose (fun line ->
-            if
-                not (line.StartsWith("CheckDeclarations.CheckOneImplFile", StringComparison.Ordinal))
-                && not (line.StartsWith("CheckDeclarations.CheckOneSigFile", StringComparison.Ordinal))
-            then
-                None
-            else
+        use csvFile = CsvFile.Load(timingCsv.FullName).Cache()
 
-            let columns = line.Split(',')
-            let duration = columns.[3] |> float |> TimeSpan.FromSeconds
-            Some duration
-        )
-        |> Array.indexed
-        |> dict
+        let csvLines =
+            csvFile.Rows
+            |> Seq.choose (fun row ->
+                let name = row.GetColumn "Name"
+
+                if
+                    not (name = "CheckDeclarations.CheckOneImplFile")
+                    && not (name = "CheckDeclarations.CheckOneSigFile")
+                then
+                    None
+                else
+
+                let duration = row.GetColumn "Duration(s)" |> float |> TimeSpan.FromSeconds
+                let fileName = row.GetColumn "fileName"
+
+                fileNamesMap
+                |> Seq.tryFind (fun (KeyValue(_, f)) -> f = fileName)
+                |> Option.map (fun kv -> kv.Key, duration)
+            )
+            |> Seq.sortBy fst
+            |> Seq.toArray
+
+        assert (csvLines.Length = graphLines.Length)
+
+        csvLines |> dict
+
+    assert (fileNamesMap.Count = timings.Count)
 
     let lastIndex = timings.Keys |> Seq.max
 
@@ -142,7 +144,6 @@ let processProject (fsprojFile: FileInfo) =
                 Idx = idx
                 FileName = fileNamesMap.[idx]
                 Duration = timings.[idx]
-
             }
         )
 
@@ -153,12 +154,50 @@ let processProject (fsprojFile: FileInfo) =
         FileNames = fileNamesMap
     }
 
+let processProject (fsprojFile: FileInfo) =
+    // Create the rsp file
+    let rspFile = Path.ChangeExtension(fsprojFile.FullName, ".rsp") |> FileInfo
+
+    if not rspFile.Exists then
+        failwith $"No rsp file was found for {fsprojFile.FullName}"
+
+    let graph =
+        match tryFindOutputFlag rspFile with
+        | None -> failwith $"no --output flag found in %s{rspFile.FullName}"
+        | Some outputFlag ->
+
+        let outputPath = Path.GetFullPath(outputFlag, fsprojFile.DirectoryName)
+        Path.ChangeExtension(outputPath, ".graph.md") |> FileInfo
+
+    assert graph.Exists
+
+    let timingCsv = Path.Combine(fsprojFile.DirectoryName, "graph.csv") |> FileInfo
+
+    if not timingCsv.Exists then
+        failwith $"No timing csv found at %s{timingCsv.FullName}"
+
+    processReportAndGraph fsprojFile.Directory timingCsv graph
+
+// let project =
+//     processReportAndGraph
+//         (DirectoryInfo(@"C:\Users\nojaf\Projects\fsac-top-level-types\src\FsAutoComplete.Core"))
+//         (FileInfo(@"C:\Users\nojaf\Projects\fsac-top-level-types\src\FsAutoComplete.Core\report.csv"))
+//         (FileInfo(
+//             @"C:\Users\nojaf\Projects\fsac-top-level-types\src\FsAutoComplete.Core\obj\Debug\net6.0\FsAutoComplete.Core.graph.md"
+//         ))
+
 let project =
-    @"C:\Users\nojaf\Projects\Fable\src\Fable.Transforms\Fable.Transforms.fsproj"
-    |> FileInfo
-    |> processProject
+    processReportAndGraph
+        (DirectoryInfo(@"C:\Users\nojaf\Projects\resharper-fsharp\ReSharper.FSharp\src\FSharp\FSharp.Common"))
+        (FileInfo(@"C:\Users\nojaf\Projects\resharper-fsharp\ReSharper.FSharp\src\FSharp\FSharp.Common\report.csv"))
+        (FileInfo(
+            @"C:\Users\nojaf\Projects\resharper-fsharp\ReSharper.FSharp\src\FSharp\FSharp.Common\obj\Debug\net472\JetBrains.ReSharper.Plugins.FSharp.Common.graph.md"
+        ))
 
 project.LongestPath
+|> List.sortByDescending (fun ni -> ni.Duration.TotalMilliseconds)
+|> List.iter (fun ni -> printfn "%A | %s" ni.Duration ni.FileName)
+
 project.Duration
 project.SlowestFile
 
